18a3ab1e8c94b75fd64d0bb1c87f760e18354128
borgerandfries <austin.new1227@outlook.com>
2026-06-11 11:29:26 -0400

    Update to BM25
    Hello

--- core/rag/new_bm25.py
+++ core/rag/new_bm25.py
@@ -1,11 +1,15 @@
-import BM25
 import bm25s
 import Stemmer
 import argparse
 
+
+from config import CHROMA_DB_DIR, COLLECTION_NAME
 from pathlib import Path
 #from fastapi import FastAPI
 from core.rag.chunking import parse_pdf
+from core.rag.chunking import extract_pdf_images
+from contextlib import contextmanager
+import sys, os
 
 '''
 TO RUN
@@ -13,27 +17,52 @@ IN TERMINAL
  python -m core.rag.new_bm25
 
 '''
+
+#Purely for testing purposes
+#Suppresses printing to the console 
+#This is done because extract_pdf_images wants to print everything
+
+@contextmanager
+def suppress_stdout():
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:  
+            yield
+        finally:
+            sys.stdout = old_stdout
+
 def getCorpus():
     print("Getting corpus...")
     #searches for PDFs in the documents folder
     corpusGet = []
+    corpusPicGet = []
     
     BASE_DIR = Path.cwd()
     for i in Path(BASE_DIR / "documents").glob("*.pdf"):
+        #Gives corpusGet the page text & page number
         corpusGet.append(parse_pdf(i))
+        #Gives corpusGet the information on the images present on the page
+        with suppress_stdout():
+            corpusPicGet.append(extract_pdf_images(i))
 
+    #Seperates the text, page numbers, and the picture data into 2 lists and one list of dictionaries 
     corpusText = []
-    corpusData = []
+    corpusPageNum = []
+    corpusPicData = []
     
     for pageDict in corpusGet:
         for j in pageDict:
             corpusText.append(j["text"])
 
-    for pageDict in corpusGet:
-        for j in pageDict:
-            corpusData.append(j["page"])
+    for textDict in corpusGet:
+        for x in textDict:
+            corpusPageNum.append(x["page"])
+
+    for picInfo in corpusPicGet:
+        corpusPicData.extend(picInfo)
     
-    return corpusText, corpusData
+    return corpusText, corpusPageNum, corpusPicData 
 
 #This function is meant purely for the test testing
 #Please remove when you are ready to make use of this
@@ -81,7 +110,7 @@ def ranker(queryTokens, corpusIndex):
     
     return results, scores
 
-def finalResults(results, scores, corpusText, corpusData):
+def finalResults(results, scores, corpusText, corpusData, picData):
     print("Final results:")
     #stolen from the BM25s GitHub readme
     #prints the ranked results
@@ -89,12 +118,17 @@ def finalResults(results, scores, corpusText, corpusData):
         doc, score = results[0, i], scores[0, i]
         print(f"\nRank {i+1} (score: {score:.2f}): {doc} \n")
         #print('\n' + corpusText[doc])
+        #print(str(picData[doc]))
         print("Page number: " + str(corpusData[doc]))
+
+        for x in picData:
+            if x["page"] == corpusData[doc]:
+                print(x)
 
 def main():
     #Main function
     #executes the above functions w/ the parameters 
-    corpus, pageNum = getCorpus()
+    corpus, pageNum, picData = getCorpus()
     #query = getQuery()
     
     #Testing query 
@@ -103,7 +137,7 @@ def main():
     corpusIndex = tokenizeCorpus(corpus)
     queryTokens = tokenizeQuery(query)
     results, scores = ranker(queryTokens, corpusIndex)
-    finalResults(results, scores, corpus, pageNum)
+    finalResults(results, scores, corpus, pageNum, picData)
     print("Done")
 
 main()
